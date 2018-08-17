67260f4a39289ee285044881b94085d60429f27c
Ilya Dmitrichenko <errordeveloper@gmail.com>
2018-08-17 13:28:51 +0100

    Update pages with new config and logic to load it

--- docs/config.js
+++ docs/config.js
@@ -1,3 +1,4 @@
+var SLACK_CHANNEL_IDS = "";
 var SLACK_TEAM_URL = "https://weave-community.slack.com/";
-var AWS_LAMBDA_DEV_URL = undefined;
+var AWS_LAMBDA_DEV_URL = "https://sdn7lbjk54.execute-api.us-east-1.amazonaws.com/dev/invite";
 var AWS_LAMBDA_PROD_URL = "https://9rzbsuwwm1.execute-api.eu-west-2.amazonaws.com/prod/invite";
