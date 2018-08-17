601118a5904c519d16e36f295940f529004c36cc
Ilya Dmitrichenko <errordeveloper@gmail.com>
2018-08-17 14:12:50 +0100

    Move output files to state dir

--- generate-page-config.sh
+++ generate-page-config.sh
@@ -9,13 +9,13 @@ echo 'var SLACK_CHANNEL_IDS = "";' > docs/config.js
 echo "var SLACK_TEAM_URL = \"https://${SLACK_TEAM}.slack.com/\";" >> docs/config.js
 
 dev_url=undefined
-if [ -e output-dev.json ] ; then
-  dev_url="\"$(cat output-dev.json | jq -r .ServiceEndpoint)/invite\""
+if [ -e .serverless/output-dev.json ] ; then
+  dev_url="\"$(cat .serverless/output-dev.json | jq -r .ServiceEndpoint)/invite\""
 fi
 
 prod_url=undefined
-if [ -e output-prod.json ] ; then
-  prod_url="\"$(cat output-prod.json | jq -r .ServiceEndpoint)/invite\""
+if [ -e .serverless/output-prod.json ] ; then
+  prod_url="\"$(cat .serverless/output-prod.json | jq -r .ServiceEndpoint)/invite\""
 fi
 
 echo "var AWS_LAMBDA_DEV_URL = ${dev_url};" >> docs/config.js
