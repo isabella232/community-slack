20c5a0134b526db2644e21d52987fc45d1d13d1e
Ilya Dmitrichenko <errordeveloper@gmail.com>
2019-11-19 12:40:33 +0000

    Re-deploy

--- docs/config.js
+++ docs/config.js
@@ -1,4 +1,4 @@
 var SLACK_CHANNEL_IDS = "";
 var SLACK_TEAM_URL = "https://weave-community.slack.com/";
-var AWS_LAMBDA_DEV_URL = "https://8cg2fyzgo2.execute-api.eu-west-2.amazonaws.com/dev/invite";
-var AWS_LAMBDA_PROD_URL = "https://9rzbsuwwm1.execute-api.eu-west-2.amazonaws.com/prod/invite";
+var AWS_LAMBDA_DEV_URL = "https://xkxdf73c8f.execute-api.eu-west-2.amazonaws.com/dev/invite";
+var AWS_LAMBDA_PROD_URL = "https://x3gxuwfadk.execute-api.eu-west-2.amazonaws.com/prod/invite";
